cf4fb3cd8ce59c5f8d6a88d867e53d6a4cbd313d
masumparves5 <masumparves5@gmail.com>
2024-01-18 21:09:32 +0600

    commmit

--- app/Http/Controllers/ShopgridController.php
+++ app/Http/Controllers/ShopgridController.php
@@ -10,4 +10,12 @@ class ShopgridController extends Controller
     {
         return view('front-end.home.home');
     }
+    public function category()
+    {
+        return view('front-end.category.index');
+    }
+    public function product()
+    {
+        return view('front-end.product.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopgridController;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\CheckController;
+use App\Http\Controllers\CustomerAuthController;
 
 Route::get('/', [ShopgridController::class, 'index']) ->name('home');
+Route::get('/product-category', [ShopgridController::class, 'category']) ->name('product.category');
+Route::get('/product-detail', [ShopgridController::class, 'product']) ->name('product.detail');
+Route::get('/card/show', [CardController::class, 'index']) ->name('card.show');
+Route::get('/checkout', [CheckController::class, 'index']) ->name('checkout');
+Route::get('/customer/login', [CustomerAuthController::class, 'login']) ->name('customer.login');
+Route::get('/customer/register', [CustomerAuthController::class, 'register']) ->name('customer.register');
