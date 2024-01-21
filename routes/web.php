7faf3b5faef93040288efa1a44e9d00437cb1453
masumparves5 <masumparves5@gmail.com>
2024-01-21 20:30:23 +0600

    first Commit

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ShopgridController;
 use App\Http\Controllers\CardController;
 use App\Http\Controllers\CheckController;
 use App\Http\Controllers\CustomerAuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [ShopgridController::class, 'index']) ->name('home');
 Route::get('/product-category', [ShopgridController::class, 'category']) ->name('product.category');
@@ -13,3 +14,7 @@ Route::get('/card/show', [CardController::class, 'index']) ->name('card.show');
 Route::get('/checkout', [CheckController::class, 'index']) ->name('checkout');
 Route::get('/customer/login', [CustomerAuthController::class, 'login']) ->name('customer.login');
 Route::get('/customer/register', [CustomerAuthController::class, 'register']) ->name('customer.register');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
